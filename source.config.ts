9bd26a5b558e4c25591aa3bdaa388a6c70ffede0
Steve Simkins <stevedsimkins@gmail.com>
2025-08-07 16:18:23 -0400

    feat: Added math support

--- source.config.ts
+++ source.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
@@ -8,6 +10,7 @@ export const docs = defineDocs({
 
 export default defineConfig({
 	mdxOptions: {
-		// MDX options
+		remarkPlugins: [remarkMath],
+		rehypePlugins: (v) => [rehypeKatex, ...v],
 	},
 });
