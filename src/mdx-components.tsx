d4f0ff345b8e2e4130864406845cb726564ba172
Steve Simkins <stevedsimkins@gmail.com>
2025-08-18 15:20:21 -0400

    chore: Reorganized files and edited styling

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,12 +1,42 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
-import OZWizard from './components/oz-wizard';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
+import OZWizard from "./components/oz-wizard";
+import {
+  FileTextIcon,
+  UsersIcon,
+  ShieldIcon,
+  CodeIcon,
+  MonitorIcon,
+  PyramidIcon,
+  StarIcon,
+  LayersIcon,
+  WrenchIcon,
+  ServerIcon,
+  AnchorIcon,
+  ShieldCheckIcon,
+  RefreshCwIcon,
+  PlugIcon,
+} from "lucide-react";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     OZWizard,
+    FileTextIcon,
+    UsersIcon,
+    ShieldIcon,
+    CodeIcon,
+    MonitorIcon,
+    PyramidIcon,
+    StarIcon,
+    LayersIcon,
+    WrenchIcon,
+    ServerIcon,
+    AnchorIcon,
+    ShieldCheckIcon,
+    RefreshCwIcon,
+    PlugIcon,
     ...components,
   };
 }
