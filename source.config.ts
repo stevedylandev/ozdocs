608ffaa783b24945f314c75dc58d502ce8ff0494
Steve Simkins <stevedsimkins@gmail.com>
2025-09-01 09:42:18 -0400

    feat: add mermaid support
    Sets up initial mermaid support per #1. Still needs some adjusting to
    make themes work in dark mode.

--- source.config.ts
+++ source.config.ts
@@ -1,23 +1,24 @@
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import { remarkMdxMermaid } from "fumadocs-core/mdx-plugins";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
-  dir: "content",
+	dir: "content",
 });
 
 export default defineConfig({
-  mdxOptions: {
-    rehypeCodeOptions: {
-      fallbackLanguage: 'plaintext',
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark'
-      }
-    },
-    remarkPlugins: [remarkMath],
-    rehypePlugins: (v) => [rehypeKatex, ...v],
-  },
+	mdxOptions: {
+		rehypeCodeOptions: {
+			fallbackLanguage: "plaintext",
+			themes: {
+				light: "github-light",
+				dark: "github-dark",
+			},
+		},
+		remarkPlugins: [remarkMath, remarkMdxMermaid],
+		rehypePlugins: (v) => [rehypeKatex, ...v],
+	},
 });
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -17,6 +17,7 @@ import {
 } from "lucide-react";
 import type { MDXComponents } from "mdx/types";
 import OZWizard from "./components/oz-wizard";
+import { Mermaid } from "@/components/mdx/mermaid";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -37,6 +38,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
 		ShieldCheckIcon,
 		RefreshCwIcon,
 		PlugIcon,
+		Mermaid,
 		...components,
 	};
 }
