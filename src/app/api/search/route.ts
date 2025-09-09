f9dfaeef9770fae92431a8b301d0a430d939ad93
Steve <contact@stevedylan.dev>
2025-09-09 14:25:06 -0400

    chore: updated search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,14 @@
-import { createFromSource } from "fumadocs-core/search/server";
 import { source } from "@/lib/source";
+import { createSearchAPI } from "fumadocs-core/search/server";
+import { structure } from "fumadocs-core/mdx-plugins";
 
-export const { GET } = createFromSource(source, {
-	// https://docs.orama.com/open-source/supported-languages
+export const { GET } = createSearchAPI("advanced", {
 	language: "english",
+	indexes: source.getPages().map((page) => ({
+		title: page.data.title,
+		description: page.data.description,
+		url: page.url,
+		id: page.url,
+		structuredData: structure(page.data.content),
+	})),
 });
