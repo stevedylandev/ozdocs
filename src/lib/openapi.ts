97f16877cc28cf82e91c87b37c9098e333397349
Steve <contact@stevedylan.dev>
2025-09-09 13:59:21 -0400

    chore: updated openapi approach

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -1,8 +1,5 @@
 import { createOpenAPI } from "fumadocs-openapi/server";
 
 export const openapi = createOpenAPI({
-	// the OpenAPI schema, you can also give it an external URL.
-	input: [
-		"https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-relayer/refs/heads/main/docs/openapi.json",
-	],
+	input: ["./public/schemas/openzeppelin-relayer-openapi.json"],
 });
