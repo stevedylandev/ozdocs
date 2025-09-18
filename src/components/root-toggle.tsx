eff8ceb7179986271736fdd8f84200334ad420c6
Steve <contact@stevedylan.dev>
2025-09-18 16:45:40 -0400

    chore: update icon sizing on mobile tab switcher

--- src/components/root-toggle.tsx
+++ src/components/root-toggle.tsx
@@ -36,7 +36,7 @@ export function RootToggle({
 
 	const item = selected ? (
 		<>
-			<div className="size-9 shrink-0 md:size-5">{selected.icon}</div>
+			<div className="size-5 shrink-0">{selected.icon}</div>
 			<div>
 				<p className="text-sm font-medium">{selected.title}</p>
 				<p className="text-[13px] text-fd-muted-foreground empty:hidden md:hidden">
@@ -78,9 +78,7 @@ export function RootToggle({
 								item.props?.className,
 							)}
 						>
-							<div className="shrink-0 size-9 md:mt-1 md:mb-auto md:size-5">
-								{item.icon}
-							</div>
+							<div className="shrink-0 size-5">{item.icon}</div>
 							<div>
 								<p className="text-sm font-medium">{item.title}</p>
 								<p className="text-[13px] text-fd-muted-foreground empty:hidden">
