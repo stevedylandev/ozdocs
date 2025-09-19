5642c6932bec6688e15e9ddeb617cbe4473e7de7
Eric Nordelo <eric.nordelo39@gmail.com>
2025-09-19 14:25:47 +0200

    feat: add configurable replacements

---
+++ content/cairo-contracts/utils/constants.ts
@@ -0,0 +1,3 @@
+export const OPENZEPPELIN_INTERFACES_VERSION = "2.1.0-alpha.0";
+export const OPENZEPPELIN_UTILS_VERSION = "3.1.0-alpha.0";
+export const UMBRELLA_VERSION = "3.0.0-alpha.2";
---
+++ content/cairo-contracts/utils/replacements.ts
@@ -0,0 +1,10 @@
+import { OPENZEPPELIN_INTERFACES_VERSION, OPENZEPPELIN_UTILS_VERSION, UMBRELLA_VERSION } from "./constants";
+
+export const REPLACEMENTS = {
+  include: ['**/content/cairo-contracts/**/*.mdx'],
+  replacements: {
+    umbrella_version: UMBRELLA_VERSION,
+    openzeppelin_interfaces_version: OPENZEPPELIN_INTERFACES_VERSION,
+    openzeppelin_utils_version: OPENZEPPELIN_UTILS_VERSION,
+  }
+}
