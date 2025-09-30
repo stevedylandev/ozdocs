625846f7c0152d396ca1c333384903dce8907ed6
Eric Nordelo <eric.nordelo39@gmail.com>
2025-09-30 15:38:21 +0200

    fix: latest alpha and stable

--- content/contracts-cairo/alpha/utils/constants.js
+++ content/contracts-cairo/alpha/utils/constants.js
@@ -1,6 +1,6 @@
 export const OPENZEPPELIN_INTERFACES_VERSION = "2.1.0-alpha.0";
 export const OPENZEPPELIN_UTILS_VERSION = "3.1.0-alpha.0";
-export const UMBRELLA_VERSION = "3.0.0-alpha.1";
+export const UMBRELLA_VERSION = "3.0.0-alpha.2";
 
 export const CLASS_HASHES = {
 	AccountUpgradeableClassHash:
