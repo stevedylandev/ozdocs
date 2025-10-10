2f392ee6b43978b937590c62fa075eb5a0154d8e
Eric Nordelo <eric.nordelo39@gmail.com>
2025-10-10 11:39:43 +0200

    feat: update utils version

--- content/contracts-cairo/alpha/utils/constants.js
+++ content/contracts-cairo/alpha/utils/constants.js
@@ -1,5 +1,5 @@
 export const OPENZEPPELIN_INTERFACES_VERSION = "2.1.0-alpha.0";
-export const OPENZEPPELIN_UTILS_VERSION = "3.1.0-alpha.0";
+export const OPENZEPPELIN_UTILS_VERSION = "2.1.0-alpha.0";
 export const UMBRELLA_VERSION = "3.0.0-alpha.3";
 export const CLASS_HASH_SCARB_VERSION = "2.12.2";
 
