0b135dc8374c274636f30f4a6c061331850cbda5
Steve <contact@stevedylan.dev>
2025-10-10 14:18:11 -0400

    fix: fixed lint error:

--- src/components/layout/docs-layout-client.tsx
+++ src/components/layout/docs-layout-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import type { ReactNode } from "react";
 import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
 import { useMemo } from "react";
 import { baseOptions } from "@/app/layout.config";
 import {
